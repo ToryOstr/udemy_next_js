5ae5df684bc8aafe30a9c3421acb999c407fe3a5
Viktoriia Ostrishko <vik.ostrishko@gmail.com>
2024-02-12 17:51:51 +0200

    add NewPost form component

--- vite-project/src/components/PostsList.jsx
+++ vite-project/src/components/PostsList.jsx
@@ -1,11 +1,16 @@
 import Post from "./Post";
+import NewPost from "./NewPost"
 import styles from './PostsList.module.css'
 export default function PostsList() {
   return (
-    <ul className={styles.posts}>
+    <>
+      <NewPost/>
+      <ul className={styles.posts}>
       <Post author="Tory" body="Checkout the full course" />
       <Post author="Nick" body="Just learn react.js" />
       <Post author="Dean" body="Learn Next.js" />
     </ul>
+    </>
+    
   );
 }

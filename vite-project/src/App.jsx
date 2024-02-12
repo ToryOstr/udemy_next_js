febe7cbe5a92bc0ee555e2d2d4fe6d18b4c94308
Viktoriia Ostrishko <vik.ostrishko@gmail.com>
2024-02-12 17:25:55 +0200

    finished lesson 20. Add some css styles

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,12 @@
 import Post from "./components/Post";
 function App() {
-  return <Post/>
+  return (
+    <main>
+      <Post author='Tory' body='Checkout the full course'/>
+      <Post author='Nick' body='Just learn react.js'/>
+      <Post author='Dean' body='Learn Next.js'/>
+    </main>
+  );
 }
 
 export default App;
--- vite-project/src/components/Post.jsx
+++ vite-project/src/components/Post.jsx
@@ -1,6 +1,9 @@
-export default function Post() {
-  return <div>
-    <p>User name</p>
-    <p>React is awesom!</p>
-  </div>
+import styles from './Post.module.css';
+export default function Post(props) {
+  return (
+    <div className={styles.post}>
+      <p className={styles.author}>{props.author}</p>
+      <p className={styles.text}>{props.body}</p>
+    </div>
+  );
 }

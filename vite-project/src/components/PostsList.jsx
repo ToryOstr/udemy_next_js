61763252708329ec7f549d04f36252d70d5cb958
Viktoriia Ostrishko <vik.ostrishko@gmail.com>
2024-02-13 12:15:22 +0200

    update: lifted state

--- vite-project/src/components/PostsList.jsx
+++ vite-project/src/components/PostsList.jsx
@@ -1,16 +1,27 @@
+import { useState } from "react";
 import Post from "./Post";
 import NewPost from "./NewPost"
 import styles from './PostsList.module.css'
+
 export default function PostsList() {
+  const [enteredtext, setEnteredText] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  function textChangeHandler() {
+    setEnteredText(event.target.value);
+  }
+
+  function authorChangeHandler() {
+    setEnteredAuthor(event.target.value);
+  }
   return (
     <>
-      <NewPost/>
+      <NewPost onTextChange={textChangeHandler} onAuthorChange={authorChangeHandler} />
       <ul className={styles.posts}>
-      <Post author="Tory" body="Checkout the full course" />
-      <Post author="Nick" body="Just learn react.js" />
-      <Post author="Dean" body="Learn Next.js" />
-    </ul>
+        <Post author={enteredAuthor} body={enteredtext} />
+        <Post author="Nick" body="Just learn react.js" />
+        <Post author="Dean" body="Learn Next.js" />
+      </ul>
     </>
-    
   );
 }

bd78846ed698817726aea5ca7b086cfbfc20b934
Viktoriia Ostrishko <vik.ostrishko@gmail.com>
2024-03-01 12:23:01 +0200

    add async function first rendering with data from backend. use hook useEffect for controling render

--- vite-project/src/components/PostsList.jsx
+++ vite-project/src/components/PostsList.jsx
@@ -2,11 +2,20 @@ import Post from "./Post";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 import styles from "./PostsList.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function PostsList({isPosting, onStopPosting}) {
 
   const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+    }
+    fetchPosts();
+  }, []);
 
   function addPostHandler(postData) {
     fetch('http://localhost:8080/posts', {

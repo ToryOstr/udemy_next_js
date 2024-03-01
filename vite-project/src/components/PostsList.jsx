5e92d7bd84a1ca877040ee220f2943b8cd7dfa56
Viktoriia Ostrishko <vik.ostrishko@gmail.com>
2024-03-01 11:46:14 +0200

    add fetch method

--- vite-project/src/components/PostsList.jsx
+++ vite-project/src/components/PostsList.jsx
@@ -9,6 +9,13 @@ export default function PostsList({isPosting, onStopPosting}) {
   const [posts, setPosts] = useState([]);
 
   function addPostHandler(postData) {
+    fetch('http://localhost:8080/posts', {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        'Content-Type': "application/json"
+      }
+    });
     setPosts((existingPosts) => [postData, ...existingPosts]);
   }
 
